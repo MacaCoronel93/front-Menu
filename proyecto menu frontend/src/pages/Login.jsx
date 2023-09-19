1a93bfd124fd363687fb6c8d0b1bb91375093838
fede pistan <pistan387@gmail.com>
2023-09-18 21:39:48 -0300

    añadimos diseño de  pagina de login

--- proyecto menu frontend/src/pages/Login.jsx
+++ proyecto menu frontend/src/pages/Login.jsx
@@ -1,14 +1,38 @@
-export default function LoginPage(){
-    return (
-         <div>
-             <h1>Login Page</h1>
-             <hr/>
-             <h3>Login</h3>
-             <form action="">
-                 <input type="text" placeholder="Email"/>
-                 <input type="password" placeholder="Password"/>
-                 <button>Login</button>
-             </form>
-         </div>
+import "./formulario.css"
+import{ useState } from "react"
+export function LoginPage(){
+  
+      const [nombre,setNombre] = useState("")
+      const [contraseña, setContraseña] = useState("")
+       const [error,setError] = useState(false)
+
+       const handleSubmit = (e) =>{
+        e.preventDefault()
+        
+        if(nombre === "" || contraseña === ""){
+            setError(true)
+            return
+        }
+       }
+    return(
+        <section className="contenedor">
+                      <h1>LOGIN</h1>
+                
+                 <form className="formulario">
+                        <div className="logo">
+                        <img src="./src/assets/logo1.png" style={{width:"140px"}}/>
+                        </div>
+                        <label>Email</label>
+                        <input type="text" placeholder="Escribe tu usuario"/>
+                        <label>Contraseña</label>
+                        <input type ="password" placeholder="Escribe tu contraseña"/>
+                        <button className="boton">Iniciar seccion</button>
+
+                        <p>No tienes cuenta?</p>
+                        <a href="#">Resgistrate</a>
+                       
+                        </form>    
+     
+        </section>
     )
 }
